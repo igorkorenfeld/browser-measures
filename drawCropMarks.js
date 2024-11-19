8d884910e5ba42ab5c2fc4a21779270914ee871a
igorkorenfeld <52677065+igorkorenfeld@users.noreply.github.com>
2024-11-19 00:08:01 -0500

    Inital commit of working drawCropmarks

---
+++ drawCropMarks.js
@@ -0,0 +1,69 @@
+// let el = document.querySelector('a[href="/wiki/Seven_Summits"]');
+// let rect = el.getBoundingClientRect(); 
+
+let running = true;
+const marks = [];
+
+function drawNearest(e) {
+  if (!running) return;
+  e.preventDefault();
+  let ex = e.clientX;
+  let ey = e.clientY;
+  let el = document.elementFromPoint(ex, ey)
+  // console.log("Element position:");
+  // console.log(document.elementsFromPoint(ex, ey));
+  // console.log(el);
+  // console.log("Mouse position:");
+  // console.log(ex, ey);
+  let rect = el.getBoundingClientRect();
+  let vCropmark = document.createElement("div");
+  let tCropmark = document.createElement("div");
+  let bCropmark = document.createElement("div");
+  marks.push(vCropmark, tCropmark, bCropmark);
+
+  // console.log("Rect");
+  // console.log(rect);
+
+  //Left keyline
+  vCropmark.style.position = "absolute";
+  vCropmark.style.top = `0`;
+  vCropmark.style.left = `${rect.left + window.scrollX}px`;
+  vCropmark.style.width = "1px";
+  vCropmark.style.height = "100vh";
+  vCropmark.style.borderLeft = "1px solid rgba(250, 0, 250, .5)";
+
+  //Top keyline
+  tCropmark.style.position = "absolute";
+  tCropmark.style.top = `${rect.top + window.scrollY}px`;
+  tCropmark.style.left = `0`;
+  tCropmark.style.width = "100vw";
+  tCropmark.style.height = "1px";
+  tCropmark.style.borderTop = "1px solid rgba(250, 0, 250, .5)";
+
+  //Bottom keyline
+  bCropmark.style.position = "absolute";
+  bCropmark.style.top = `${rect.bottom + window.scrollY}px`;
+  bCropmark.style.left = `0`;
+  bCropmark.style.width = "100vw";
+  bCropmark.style.height = "1px";
+  bCropmark.style.borderTop = "1px solid rgba(250, 0, 250, .5)";
+
+  document.body.appendChild(vCropmark);
+  document.body.appendChild(tCropmark);
+  document.body.appendChild(bCropmark);
+}
+
+function removeMarks(e) {
+  if (e.code === 'KeyX')  {
+   marks.forEach((mark) => document.body.removeChild(mark));
+    document.removeEventListener("click", drawNearest);
+    document.removeEventListener("keyup", removeMarks);
+    }
+  else if (e.code === 'KeyP') {
+    running = !running; 
+  }
+}
+
+document.addEventListener("click", drawNearest);
+document.addEventListener("keyup", removeMarks);
+
